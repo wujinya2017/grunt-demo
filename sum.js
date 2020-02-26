e5465d5ef69caa1310c53b6ea2dcf956062b393c
wujinya <2601519403@qq.com>
2020-02-26 12:32:29 +0800

    mocha-be

--- sum.js
+++ sum.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 if(process.argv.length != 4){
-  console.log('sun x y');
+  console.log('sum x y');
   process.exit(1);
 }
 var x=Number(process.argv[2]);
 var y=Number(process.argv[3]);
 
-console.log('${x}+${y}=${x+y}');
+const add = require('./calc.js');
 
-function add(x,y){
-  if((typeof x) === 'number' && (typeof y) === 'number'){
-    return x+y
-  } else{
-    return NaN
-  }
-}
+console.log(`${x} + ${y} = ${add(x,y)}`);
+
