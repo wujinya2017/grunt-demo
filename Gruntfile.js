766612d3edf7ba93f0a12cc2f4bb189afa8fb296
wujinya <2601519403@qq.com>
2020-03-03 15:14:42 +0800

    ug

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,16 @@
+
 module.exports = function (grunt) {
   grunt.initConfig({
-    csslint: {
-      options: {
-        csslintrc: './.csslintrc'
-      },
-      src: ['*.css']
+    uglify: {
+      release:{
+        files: {
+          'js/jquery.min.js': 'js/jquery.js'
+        }
+      }       
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['csslint']);
+  grunt.registerTask('default', ['uglify:release']);
 };
-
