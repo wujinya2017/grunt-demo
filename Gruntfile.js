00fa7d7140a12a965054abbb5c4b53525bcef3c7
wujinya <2601519403@qq.com>
2020-03-04 16:44:01 +0800

    imagesmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,17 @@
-
+/*global module*/
 module.exports = function (grunt) {
   grunt.initConfig({
-    uglify: {
-      release:{
-        files: {
-          'js/jquery.min.js': 'js/jquery.js'
-        }
-      }       
+    imagemin: {                               
+      files: {
+        expand: true,
+        src: ['images/*.{png,jpg,gif}'],
+        dest: 'dist/'
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['uglify:release']);
+  grunt.registerTask('default', ['imagemin']);    
 };
+
